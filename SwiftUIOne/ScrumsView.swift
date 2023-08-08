0e7a6abab31ff8e4d3bfff396d8fad472efa0db8
mk812y <mk812y@gmail.com>
2023-08-08 20:50:26 +0200

    ScrumsView

---
+++ SwiftUIOne/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 8.8.23..
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
