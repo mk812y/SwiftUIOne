a466c456ded5c186cd3026fc0a1c45f6b05f535b
mk812y <mk812y@gmail.com>
2023-08-08 18:32:01 +0200

    ok

---
+++ SwiftUIOne/Model/DailyScrum.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 8.8.23..
+//
+
+import Foundation
---
+++ SwiftUIOne/Model/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 8.8.23..
+//
+
+import Foundation
