af694f9a93d54aa6f651ca21659a9e3245905620
mk812y <mk812y@gmail.com>
2023-08-11 16:12:36 +0200

    Display attendees in the edit view

---
+++ SwiftUIOne/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 11.8.23..
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
