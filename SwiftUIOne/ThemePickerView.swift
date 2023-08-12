9e513d35d191a0bfaac6059f174c92e03864dd5d
mk812y <mk812y@gmail.com>
2023-08-12 20:08:19 +0200

    Section 3 Pass the edit view a binding to data

---
+++ SwiftUIOne/ThemePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemePickerView.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 12.8.23..
+//
+
+import SwiftUI
+
+struct ThemePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemePickerView()
+}
