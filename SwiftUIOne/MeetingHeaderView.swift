8255240720a53c8b38f262da1cf874350876c07e
mk812y <mk812y@gmail.com>
2023-08-13 20:35:05 +0200

    Section 3 Add design elements to the meeting header

---
+++ SwiftUIOne/MeetingHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingHeaderView.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 13.8.23..
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingHeaderView()
+}
