10676f7ae98e95da220bdcae993a00f1cddf2bd1
mk812y <mk812y@gmail.com>
2023-08-13 20:35:45 +0200

    ok

--- SwiftUIOne/MeetingHeaderView.swift
+++ SwiftUIOne/MeetingHeaderView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("\(minutesRemaining) minutes")
+        }
     }
 }
 
 #Preview {
-    MeetingHeaderView()
+    MeetingHeaderView(secondsElapsed: 10, secondsRemaining: 5)
 }
+
+//https://developer.apple.com/tutorials/app-dev-training/managing-state-and-life-cycle
+//Section 3
+//Add design elements to the meeting header
