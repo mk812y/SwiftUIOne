c7bb8f327db84e487edf4254df87e335a7407a8a
mk812y <mk812y@gmail.com>
2023-08-14 18:22:28 +0200

    ok2

---
+++ SwiftUIOne/MeetingFooterView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingFooterView.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 14.8.23..
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingFooterView()
+}
