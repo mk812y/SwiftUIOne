ea7ef1044f648cf2d884875e31f7c68e77babbb0
mk812y <mk812y@gmail.com>
2023-08-16 21:26:35 +0200

    ok

---
+++ SwiftUIOne/NewScrumSheet.swift
@@ -0,0 +1,18 @@
+//
+//  NewScrumSheet.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 16.8.23..
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewScrumSheet()
+}
