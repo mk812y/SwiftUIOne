55535bc8c201424477a8e498be9dc3b7ba88b84a
mk812y <mk812y@gmail.com>
2023-08-26 18:51:29 +0200

    ок

---
+++ SwiftUIOne/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 26.8.23..
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
---
+++ SwiftUIOne/Model/ErrorWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorWrapper.swift
+//  SwiftUIOne
+//
+//  Created by Михаил Куприянов on 26.8.23..
+//
+
+import Foundation
