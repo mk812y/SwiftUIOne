ef9fe1ff32cca4b3f231e5d1cb67944a04399d56
mk812y <mk812y@gmail.com>
2023-09-02 18:59:24 +0200

    Section 3 done

--- SwiftUIOne/ErrorView.swift
+++ SwiftUIOne/ErrorView.swift
@@ -9,21 +9,31 @@ import SwiftUI
 
 struct ErrorView: View {
     let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack {
-            Text("An error has occurred")
-                .font(.title)
-                .padding(.bottom)
-            Text(errorWrapper.error.localizedDescription)
-                .font(.headline)
-            Text(errorWrapper.guidance)
-                .font(.caption)
-            Spacer()
+        NavigationStack {
+            VStack {
+                Text("An error has occurred")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
         }
-        .padding()
-        .background(.ultraThinMaterial)
-        .cornerRadius(16)
     }
 }
 
