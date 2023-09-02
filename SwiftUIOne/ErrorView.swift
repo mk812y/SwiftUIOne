7323d62c0bad6e66f81b08a5cd5266c3d0185a7d
mk812y <mk812y@gmail.com>
2023-09-02 18:46:23 +0200

    section 2 done

--- SwiftUIOne/ErrorView.swift
+++ SwiftUIOne/ErrorView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("An error has occurred")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
     }
 }
 
-#Preview {
-    ErrorView()
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
 }
--- SwiftUIOne/SwiftUIOneApp.swift
+++ SwiftUIOne/SwiftUIOneApp.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 @main
 struct SwiftUIOneApp: App {
-    @State private var scrums = DailyScrum.sampleData
+    @StateObject private var store = ScrumStore()
     
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $scrums)
+            ScrumsView(scrums: $store.scrums) {
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+            .task {
+                do {
+                    try await store.load()
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+            }
         }
     }
 }
 
 
-//https://developer.apple.com/tutorials/app-dev-training/adopting-swift-concurrency
+//
